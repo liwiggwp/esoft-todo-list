67186610f7bc3d87419028e712d02e0b1e61e701
Venera Kodo <liwiggwp@gmail.com>
2025-04-17 13:50:09 +0500

    add data task and get tasks by api

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const { tasks } = require("./data/task");
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/tasks", (req, res) => {
+  res.json(tasks);
+});
+
+app.get("/tasks/:id", (req, res) => {
+  const task = tasks.find((t) => t.id === parseInt(req.params.id, 10));
+  if (task) {
+    res.json(task);
+  } else {
+    return res.status(404).json({ message: "Задача не найдена" });
+  }
 });
 
 const PORT = 5000;
